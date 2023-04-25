09525bdaa82dbbdd509b4d7cd37c75a654a0a5d6
Mateusz-11 <tweak.night@gmail.com>
2023-04-25 21:32:21 +0200

    Part1 - List of notes

--- reactnotes/src/Notes.js
+++ reactnotes/src/Notes.js
@@ -2,6 +2,20 @@ import React from "react";
 
 import { Table } from "react-bootstrap";
 
+const noteList = [
+	{
+		title: "Go to the gym",
+		category: "Hobby",
+		content: "Leg day",
+		date: new Date("2023-04-25"),
+	},
+	{
+		title: "Go to the library",
+		category: "Education",
+		content: "Poetry",
+		date: new Date("2023-04-26"),
+	},
+];
 function Notes() {
 	const header = <h3>List of notes</h3>;
 	return (
